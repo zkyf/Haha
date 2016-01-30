86331a8b4ee13ae0635b00998a63d956b3a1dca0
zkzkz <ljxforgaokao@163.com>
2016-01-31 02:03:43 +0800

    Created

---
+++ test/hama.h
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+
+using namespace std;
+
+#ifndef _HAMA_H
+#define _HAMA_H
+
+//保留字
+#define 资词不资词 if
+#define 董先生 true
+#define 彭定康 false
+#define 没有任何这样的意思 else
+#define 我就不知道 while
+#define 到交大 using
+#define 做个报告 namespace
+#define 特首 this
+#define 基本法 class
+#define 香港的法律 struct
+#define 按照 ::
+#define 我们的决定权 private
+#define 去产生 public
+#define 你们搞的这个 operator
+
+//常用DS
+#define 闷声大发财 void
+#define 也是很大的 int
+#define 一点微小的贡献 double
+#define 任何这样的意思 char*
+#define 这样的意思 char
+#define 好几百个 vector
+#define 你刚才问我这个问题 HRESULT
+#define 选举法 enum
+
+//常用运算符
+#define 学习一个 <<
+#define 报告 >>
+#define 硬点 =
+#define 钦点 ->
+#define 续命 ++
+
+//常用常量
+#define 做了一个 cin
+#define 报道出了偏差 cerr
+#define 你们新闻界 cout
+#define 华莱士 999999999
+#define 人生的经验 endl
+#define 毕竟图样 null
+
+//常用函数
+#define 中央 main
+#define 我就说资词(x) system(x)
+#define 识得唔识得噶 printf("......+1s\n"),system("pause")
+#define 一个赛艇(x) return(x)
+
+#endif
